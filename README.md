1e8ea7603de63074a7c914924798c2bbe825a932
David Yan <yandavid@seas.upenn.edu>
2024-03-24 11:12:42 -0400

    Bouncing particles

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CMake SFML Project Template
+# Fluid Simulation
 
 This repository template should allow for a fast and hassle-free kick start of your next SFML project using CMake.
 Thanks to [GitHub's nature of templates](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template), you can fork this repository without inheriting its Git history.
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,61 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+std::vector<sf::CircleShape> particles;
+std::vector<sf::Vector2f> particleVelocities;
+
+void spawnParticles(const int rows, const int cols, const float radius, const sf::Vector2f &center) {
+    int nParticles = rows * cols;
+    int rowCenterIdx = rows / 2;
+    int colCenterIdx = cols / 2;
+    if (rows % 2 == 1) {
+        rowCenterIdx += 1;
+    }
+    if (cols % 2 == 1) {
+        colCenterIdx += 1;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sf::CircleShape particle(radius);
+            particle.setFillColor(sf::Color(0, 0, 255));
+
+            sf::Vector2f displacement((i - rowCenterIdx) * 2 * radius, (j - colCenterIdx) * 2 * radius);
+            particle.setPosition(center + displacement);
+            particles.push_back(particle);
+            particleVelocities.push_back(sf::Vector2f(0.f, 0.f));
+        }
+    }
+}
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+
+    auto window = sf::RenderWindow{ { 1280u, 720u }, "Fluid Sim Project" , sf::Style::Default, settings};
+    window.setFramerateLimit(60);
+
+    // get the size of the window
+    sf::Vector2u size = window.getSize();
+    unsigned int width = size.x;
+    unsigned int height = size.y;
+
+    float circleRadius = 5;
+    // sf::CircleShape circle(circleRadius);
+    // // set the shape color to blue
+    // circle.setFillColor(sf::Color(0, 0, 255));
+    // // set the number of sides (quality)
+    // // circle.setPointCount(100);
+    // circle.setPosition(width/2, height/2);
+    // sf::Vector2f circleVelocity(5.f, 0.f);
+
+    spawnParticles(10, 10, circleRadius, sf::Vector2f(float(width)/2.f, float(height)/2.f));
+
+    sf::Vector2f gravity(0.f, 9.8);
+    sf::Vector2f damping(0.9, 0.9);
+
+    sf::Clock clock;
 
     while (window.isOpen())
     {
@@ -16,6 +68,31 @@ int main()
         }
 
         window.clear();
+
+        sf::Time elapsed = clock.restart();
+        for (int i = 0; i < particles.size(); i++) {
+            sf::CircleShape &particle = particles[i];
+            sf::Vector2f &particleVelocity = particleVelocities[i];
+
+            particle.move(particleVelocity);
+
+            // Check particle bounds
+            sf::Vector2f updatedPosition = particle.getPosition();
+            if (updatedPosition.x < 0 || updatedPosition.x + (2 * circleRadius) >= width) {
+                particleVelocity.x *= -1 * damping.x;
+            }
+            if (updatedPosition.y < 0 || updatedPosition.y + (2 * circleRadius) >= height) {
+                particleVelocity.y *= -1 * damping.y;
+            }
+            float boundedX = std::min(std::max(updatedPosition.x, 0.f), float(width - (2 * circleRadius)));
+            float boundedY = std::min(std::max(updatedPosition.y, 0.f), float(height - (2 * circleRadius)));
+            particle.setPosition(boundedX, boundedY);
+
+            particleVelocity += gravity * elapsed.asSeconds();
+
+            window.draw(particle);
+        }
+        
         window.display();
     }
 }
